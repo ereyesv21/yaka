8e67b82edcc56659f58de121e9ae23f0a3514803
Elid <ereyes@uniempresarial.edu.co>
2025-02-21 18:37:40 -0500

    Descripción de los cambios

--- dist/inicio.js
+++ dist/inicio.js
@@ -6,7 +6,9 @@ let sliderList = slider.querySelector('.deslizador .lista')
 let thumbnail = document.querySelector('.deslizador .miniatura')
 let thumbnailItems = thumbnail.querySelectorAll('.elemento')
 
-thumbnail.appendChild(thumbnailItems[0])
+/* thumbnail.appendChild(thumbnailItems[0]) */
+
+
 
 // Function for next button 
 nextBtn.onclick = function() {
@@ -44,6 +46,43 @@ function moveSlider(direction) {
     }, {once: true}) // Remove the event listener after it's triggered once
 }
 
+
+let puntos = document.querySelectorAll('.punto');
+
+function actualizarIndicador() {
+    let indiceActivo = Array.from(thumbnailItems).indexOf(thumbnail.querySelector('.elemento:first-child'));
+    puntos.forEach((punto, index) => {
+        punto.classList.toggle('activo', index === indiceActivo);
+    });
+}
+
+// Llama a la función para inicializar el indicador
+actualizarIndicador();
+
+// Modifica la función moveSlider para actualizar el indicador después de mover el slider
+function moveSlider(direction) {
+    let sliderItems = sliderList.querySelectorAll('.elemento');
+    let thumbnailItems = document.querySelectorAll('.miniatura .elemento');
+    
+    if(direction === 'derecha'){
+        sliderList.appendChild(sliderItems[0]);
+        thumbnail.appendChild(thumbnailItems[0]);
+        slider.classList.add('derecha');
+    } else {
+        sliderList.prepend(sliderItems[sliderItems.length - 1]);
+        thumbnail.prepend(thumbnailItems[thumbnailItems.length - 1]);
+        slider.classList.add('izquierda');
+    }
+
+    slider.addEventListener('animationend', function() {
+        if(direction === 'derecha'){
+            slider.classList.remove('derecha');
+        } else {
+            slider.classList.remove('izquierda');
+        }
+        actualizarIndicador(); // Actualiza el indicador después de la animación
+    }, {once: true});
+}
 
 
 
